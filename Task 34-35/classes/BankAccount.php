07059b8585a922b382c86851c6183b77b6470024
YPandor <53355278+YPandor@users.noreply.github.com>
2020-02-02 23:51:27 +0000

    Add files via upload

---
+++ Task 34-35/classes/BankAccount.php
@@ -0,0 +1,33 @@
+<?php
+
+class BankAccount implements IfaceBankAccount
+{
+
+    private $balance = null;
+
+    public function __construct(Money $openingBalance)
+    {
+        $this->balance = $openingBalance;
+    }
+
+    public function balance()
+    {
+        return $this->balance;
+    }
+
+    public function deposit(Money $amount)
+    {
+        //implement this method
+        $this->balance = $this->balance->value() + $amount->value();
+    }
+
+    public function transfer(Money $amount, BankAccount $account)
+    {
+        //implement this method
+        $this->balance = $this->balance - $amount->value();
+        $account->balance = $account->balance + $amount->value();
+    }
+    public function withdraw(Money $amount) {
+        $this->balance = $this->balance->value() - $amount->value();
+    }
+}
